a214f37ec0abc310d2f42e0421b930cc0974ba0d
amarchoudhary-usf <amar.choudhary@us.inc>
2025-12-12 14:03:02 +0530

    fix bugs remaining

--- startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start all services in parallel
-python app.py &
-python agent.py dev &
-python ./outboundService/entry.py dev &
-
-# Keep script running until all background jobs exit
-wait
